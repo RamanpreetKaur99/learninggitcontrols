ab56d08257b00dd9dd61a70e4cad906fa4fc4e3d
Ramanpreet Kaur <rpks2003m@gmail.com>
2022-01-15 22:12:57 +0530

    main file in b-1 committed

--- src/main/java/com/java/testing/gitcontrol/GitcontrolApplication.java
+++ src/main/java/com/java/testing/gitcontrol/GitcontrolApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitcontrolApplication {
 
 	// Comment added in branch B-1
+	private int i = 1;
+	// Comment added in branch main
 	public static void main(String[] args) {
 		SpringApplication.run(GitcontrolApplication.class, args);
 	}
