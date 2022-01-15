ed3d667fb98389189685868b31a4896caaab7032
Ramanpreet Kaur <rpks2003m@gmail.com>
2022-01-15 21:54:30 +0530

    basic project committed

---
+++ src/main/java/com/java/testing/gitcontrol/GitcontrolApplication.java
@@ -0,0 +1,13 @@
+package com.java.testing.gitcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitcontrolApplication.class, args);
+	}
+
+}
